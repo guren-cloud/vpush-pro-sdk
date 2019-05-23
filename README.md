0e0903d0fcf59e7a5ee02da4a285cf86e6881cf1
safe-dog <admin@safedog.cc>
2019-05-23 16:22:37 +0800

    Add vPush2.0/Basic sdk

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 ## vPush SDK Pro
 
-> 高级版本专用    
-> https://mssnn.cn    
-> 详细文档：http://doc.mssnn.cn
+> 此SDK包含了高级版、专业版、云开发版、黑科技版等核心模块。    
+> 你可以根据自己使用的推送平台，引入对应的模块文件。
+
+## 高级版
+> 官网：https://mssnn.cn
+> 文档：http://doc.mssnn.cn
+
+对应核心文件：`vpush.pro.js`
+
+## 专业版
+> 官网：https://vpush.cloud
+> 文档：https://www.kancloud.cn/mssnn_cn/vpush/1068329
+
+对应核心文件：
+1. `vpush2.basic.js` 专业基础版
+2. 更新中..
 
 ## 示例
 小程序代码片段地址：[https://developers.weixin.qq.com/s/905pzCma704O](https://developers.weixin.qq.com/s/905pzCma704O)    
@@ -10,11 +23,13 @@
 > 导入请务必填写正确的AppId，以及修改`app.js`中`new vPush`定义的AppId
 
 ## 配置域名
-我们为每一个开发者都配置了独立的子域名，你可以直接在浏览器中输入：
+高级版对应域名为你的开发者控制台地址：
 [https://你的小程序AppId.mssnn.cn](https://mssnn.cn)    
-进入开发控制台（初次访问需要进行简单注册）    
 
-同时，这个地址也是收集推送凭证等后端API服务地址，所以我们前往微信公众号平台，在开发设置里配置request域名为此。
+专业版对应域名为你搭建好的API服务地址，比如：
+[https://vpush2-one.mssnn.cn](https://vpush2-one.mssnn.cn)
+
+前往微信公众号平台，在开发设置里配置request域名为此。
 
 ## 配置SDK
 
@@ -23,7 +38,7 @@
 $ git clone https://github.com/guren-cloud/vpush-pro-sdk
 ```
 
-### 2. 编辑`app.js`，引入sdk的`vpush.pro.js`文件，并初始化一个全局的`vPush`变量：
+### 2. 编辑`app.js`，引入sdk的模块文件（高级版选择`vpush.pro.js`，专业版选择其他对应的模块），并初始化一个全局的`vPush`变量：
 
 ``` js
 // app.js
@@ -37,6 +52,7 @@ App({
 ```
 
 > 初始化vPush类，需要传递你当前的微信小程序AppId
+> 如果是高级版，请传递你的API域名，如`new vPush('https://vpush2-one.mssnn.cn')`
 
 ### 3. 使用sdk的组件
 > 在sdk的`components`里，我们预置了部分组件，让你只需要经过简单的配置使用即可完成自动收集推送凭证流程。
@@ -65,11 +81,3 @@ App({
 这个组件会每天用户打开小程序的时候，弹出一个签到框，用户点击签到按钮即可自动收集凭证。 无需过多开发。
 
 [**更详细的文档，请移至：http://doc.mssnn.cn**](http://doc.mssnn.cn)
-
-
-## 旧版本迁移
-如果你的小程序项目中已经配置过旧版本（目前称之为社区版）的SDK，那么除了重新配置外，还可以通过稍微修改，以达到兼容的目的。
-
-1. 复制新版本`vpush.pro.js`的文件内容；
-2. 编辑旧版本`_vpush.js`文件，全选粘贴覆盖旧版本文件内容；
-3. 覆盖完毕，在编辑好的`vpush.pro.js`文件底部的`module.exports = vPush;`代码，改成`module.exports = new vPush('你的小程序AppId');`
---
+++ vpush2.basic.js
@@ -0,0 +1,235 @@
+/**
+ * vPush/2.0 Basic基础版SDK
+ * -------------
+ * 独立部署版专用SDK
+ * 网址：https://vpush.cloud
+ * =============
+ * https://github.com/guren-cloud/vpush-pro-sdk
+ * 更新时间：2019/05/23
+ * -------------
+ * 使用方法：
+ * - app.js中引入此模块文件，然后初始化：App({ vPush: new vPush('你的API服务接口地址'), ..})
+ * - 比如：new vPush("https://vpush2-one.mssnn.cn")
+ * 其他提示：
+ * - 这个SDK需要你配置你的小程序的request域名，为你部署好的API服务器地址。
+ */
+
+class vPush {
+  constructor(api) {
+    if (!api) {
+      throw new Error("[vPush.init] 请设置您的API服务接口地址");
+    }
+    console.log("[*] vPush/2.0 专业、高效、实用的小程序消息推送平台");
+    console.log("[+] https://vpush.cloud");
+    this.HOST = api + '/v1';
+    this.STORAGE_KEY = '_VPUSH2_BASIC_OPENID';
+    this.OPEN_ID = '';
+
+    this.init();
+  }
+
+  // http请求后端
+  _request(uri, data) {
+    return new Promise((RES, REJ) => {
+      wx.request({
+        url: `${this.HOST}${uri}`,
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data,
+        success: RES,
+        fail: REJ
+      })
+    });
+  }
+
+  /**
+   * 初始化
+   */
+  init() {
+    this.initUser().then(openid => {
+      console.log('[vpush.init.ok] open_id=', openid);
+      this.OPEN_ID = openid;
+      // 登陆，获取用户头像等信息
+      new Promise(RES => {
+        wx.getUserInfo({
+          success: ret => {
+            RES(ret.userInfo);
+          },
+          fail: err => {
+            RES({});
+          }
+        })
+      }).then(userInfo => {
+        // 获取当前操作系统等信息
+        var info = wx.getSystemInfoSync();
+        // 更新用户数据
+        this._request('/client/update?openid=' + this.OPEN_ID, {
+          // 用户昵称、头像、性别
+          nickName: userInfo.nickName || "",
+          avatarUrl: userInfo.avatarUrl || "",
+          gender: userInfo.gender || 0,
+          // 系统信息
+          system: info.system,
+          platform: info.platform,
+          version: info.version,
+          language: info.language,
+          sdk: info.SDKVersion
+        }).then(ret => {
+          // 如果用户不存在，就刷新
+          if (ret.data.errcode === -1) {
+            wx.removeStorageSync(this.STORAGE_KEY);
+            return this.init();
+          }
+          console.log('[vpush.update.ok]', ret);
+        });
+      });
+    }).catch(err => {
+      console.log('[vpush.init.err]', err);
+    })
+  }
+
+  /**
+   * 初始化用户
+   * 根据code获取openId
+   */
+  initUser() {
+    return new Promise((RES, REJ) => {
+      // 如果有缓存，返回
+      let cache = wx.getStorageSync(this.STORAGE_KEY);
+      if (cache && cache.length > 10) return RES(cache);
+      // 没有，就初始化
+      wx.login({
+        success: res => {
+          this._request('/client/init', {
+            code: res.code
+          }).then(ret => {
+            const { data } = ret;
+            console.log('[vpush.init.ret]', data);
+            if (data.errcode !== 0) {
+              return REJ(data.errmsg);
+            }
+            RES(data.openid);
+            // 存储到缓存
+            wx.setStorageSync(this.STORAGE_KEY, data.openid);
+          }).catch(REJ);
+        },
+        fail: REJ
+      })
+    });
+  }
+
+  /**
+   * 打开推送
+   */
+  openPush() {
+    return this.togglePush();
+  }
+
+  /**
+   * 关闭推送
+   */
+  closePush() {
+    return this.togglePush(false);
+  }
+
+  /**
+   * 开/关推送
+   */
+  togglePush(openPush = true) {
+    return new Promise((RES, REJ) => {
+      this._request('/client/push?openid=' + this.OPEN_ID, {
+        open: openPush ? 1 : 0
+      }).then(ret => {
+        console.log('[vpush.togglePush.ret]', ret);
+        let { data } = ret;
+        if (data.errcode === 0) {
+          console.log('[vpush.togglePush.ok]', data.message);
+          RES();
+        } else {
+          console.warn('[vpush.togglePush.fail]', data.errmsg);
+          REJ(data.errmsg);
+        }
+      })
+    })
+  }
+
+  /**
+   * 检查是否开启了推送
+   */
+  isOpenPush() {
+    if (!this.OPEN_ID) return console.warn('[vPush.isOpenPush] 尚未初始化完毕');
+    return new Promise((RES, REJ) => {
+      wx.request({
+        url: this.HOST + '/client/push?openid=' + this.OPEN_ID,
+        success: ret => {
+          console.log('[vpush.isopenpush.ret]', ret);
+          const { data } = ret;
+          if (data.errcode !== 0) {
+            console.warn('[vpush.isOpenPush.err]', data);
+            REJ(data.errmsg);
+          }
+          console.log('[vpush.isopenPush.ok]', data);
+          RES(data.openPush === 1);
+        },
+        fail: REJ
+      })
+    })
+  }
+
+  // 兼容社区版SDK
+  add(e) {
+    return this.addFormId(e);
+  }
+
+  /**
+   * 添加推送凭证
+   */
+  addFormId(e, callback) {
+    var formId = '';
+    if (typeof e === 'object') {
+      formId = e.detail.formId;
+    } else {
+      formId = String(e);
+    }
+    // 判断formId是否为测试的
+    if (formId.startsWith('the formId') || formId.startsWith('[') || formId === 'undefined') {
+      callback && callback();
+      return console.warn('[vpush] 调试的无效formId：', formId);
+    }
+
+    // 获取当前页面地址
+    let page = getCurrentPages()[0].route;
+    this._request('/client/formid?openid=' + this.OPEN_ID, {
+      page,
+      formId,
+      platform: wx.getSystemInfoSync().platform
+    }).then(ret => {
+      callback && callback();
+      console.log('[vpush.addFormId.ret]', ret);
+      const { data } = ret;
+      if (data.errcode !== 0) {
+        return console.warn('[vpush.addForm.fail]', data.errmsg);
+      }
+      console.log('[vpush.addFormId.ok]', data.message);
+    });
+  }
+
+  /**
+   * 获取用户的推送凭证统计
+   */
+  getFormIdCounts(callback) {
+    if (!this.OPEN_ID) {
+      return setTimeout(() => this.getFormIdCounts(callback), 1000);
+    }
+    wx.request({
+      url: `${this.HOST}/formids/user_counts?openId=${this.OPEN_ID}`,
+      success: ret => {
+        callback(ret.data.counts)
+      }
+    })
+  }
+}
+
+module.exports = vPush;
